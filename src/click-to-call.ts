46224c92d7531a83fb7145ccadbf507e5e9f8ae3
cubase <jakub.kone@gmail.com>
2020-01-29 10:52:42 +0100

    Implemented api for c2c init

--- src/click-to-call.ts
+++ src/click-to-call.ts
@@ -1,18 +1,49 @@
 import registerCustomElement from 'preact-custom-element'
 import { ClickToCall } from './preact'
-;(function(document): void {
-  registerCustomElement(ClickToCall, 'x-clicktocall')
+
+interface CustomWindow extends Window {
+  c2c: object
+}
+
+declare const window: CustomWindow
+
+interface C2CConfig {
+  [key: string]: string | undefined
+}
+
+window.c2c = (function(document): object {
+  const requiredParams = ['uri', 'user', 'password', 'socket', 'callto']
   const currentScript = document.currentScript
-  if (currentScript !== null && currentScript.dataset) {
-    const scriptDataset = Object.entries(currentScript.dataset)
-    if (!scriptDataset.length) {
-      return console.error(
-        '[CTC] No configuration found. Did you forget to add data attributes into this script?',
-        'https://github.com/cubase/call-to-webrtc'
+
+  function attach(dataset: C2CConfig): void {
+    // Skip rendering if button is already attached
+    if (document.querySelector('x-clicktocall')) return
+
+    if (!dataset || dataset.constructor.name !== 'Object') return
+
+    // Check dataset contains all required params
+    if (requiredParams.every(key => dataset[key] !== undefined)) {
+      registerCustomElement(ClickToCall, 'x-clicktocall')
+      const ctc = document.createElement('x-clicktocall')
+      Object.entries(dataset).forEach(([key, value]) => value && ctc.setAttribute(key, value))
+      document.body.appendChild(ctc)
+    } else {
+      console.log(
+        `[C2C] You need to provide all required parameters: ${requiredParams.join(
+          ','
+        )}. Add data attributes to script or use c2c.init() function.`
       )
     }
-    const ctc = document.createElement('x-clicktocall')
-    scriptDataset.forEach(([key, value]) => value && ctc.setAttribute(key, value))
-    document.body.appendChild(ctc)
+  }
+
+  if (currentScript !== null && currentScript.dataset) {
+    if (Object.keys(currentScript.dataset).length) {
+      // Try to attach with provided data attributes
+      attach(currentScript.dataset)
+    }
+  }
+
+  return {
+    init: (config: C2CConfig): void => attach(config)
   }
 })(document)
